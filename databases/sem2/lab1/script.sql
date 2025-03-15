07412f52dd04946491b4b8de5c7550ec2bb429b8
NF-coder <resetnikovs72@gmail.com>
2025-03-15 18:05:34 +0300

    some changes, idk exact

--- databases/sem2/lab1/script.sql
+++ databases/sem2/lab1/script.sql
@@ -2,27 +2,27 @@ BEGIN;
 
 CREATE TABLE IF NOT EXISTS ObjectType
 (
-    TypeName TEXT PRIMARY KEY
+    	TypeName TEXT PRIMARY KEY
 );
 CREATE TABLE IF NOT EXISTS Object
 (
 	ObjectId SERIAL PRIMARY KEY,
-    ObjectName TEXT NOT NULL, 
-    ObjectType TEXT NOT NULL REFERENCES ObjectType(TypeName)
+    	ObjectName TEXT NOT NULL, 
+    	ObjectType TEXT NOT NULL REFERENCES ObjectType(TypeName)
 );
 CREATE TABLE IF NOT EXISTS AvilableAction
 (
 	ActionName TEXT PRIMARY KEY,
 	ActionTargetObject INTEGER REFERENCES Object(ObjectId),
-    ActionTargetType TEXT REFERENCES ObjectType(TypeName),
-    ActionInitiator INTEGER REFERENCES Object(ObjectId),
-    CONSTRAINT EeitherTargetOrInitiator CHECK (ActionTargetObject IS NOT NULL OR ActionTargetType IS NOT NULL OR ActionInitiator IS NOT NULL)
+    	ActionTargetType TEXT REFERENCES ObjectType(TypeName),
+    	ActionInitiator INTEGER REFERENCES Object(ObjectId),
+    	CONSTRAINT EeitherTargetOrInitiator CHECK (ActionTargetObject IS NOT NULL OR ActionTargetType IS NOT NULL OR ActionInitiator IS NOT NULL)
 );
 CREATE TABLE IF NOT EXISTS Action
 (
 	ActionId SERIAL PRIMARY KEY,
-    Action TEXT NOT NULL REFERENCES AvilableAction(ActionName),
-    PreviousAction INTEGER REFERENCES Action(ActionId)
+   	Action TEXT NOT NULL REFERENCES AvilableAction(ActionName),
+    	PreviousAction INTEGER REFERENCES Action(ActionId)
 );
 
 INSERT INTO ObjectType(TypeName) 
@@ -39,7 +39,7 @@ VALUES
 	('Видения', 'Мысли'),
 	('Щупальца', 'Мысли');
 
-INSERT INTO AvilableAction(ActionName, ActionTargetObject, ActionTargetGroup, ActionInitiator)
+INSERT INTO AvilableAction(ActionName, ActionTargetObject, ActionTargetType, ActionInitiator)
 VALUES 
  	('Оставить в покое', NULL, 'Глупые питекантропы', 2),
 	('Сосредоточить внимание', NULL, 'Способные питекантропы', 2),
@@ -49,10 +49,10 @@ VALUES
 
 INSERT INTO Action(Action, PreviousAction)
 VALUES 
-	(1, NULL), -- Крсталл оставил некоторых питекантропов в покое
-	(2, NULL), -- Кристалл сосредоточил внимание на способных питекантропах
-	(3, NULL), -- Смотрящий на Луну почувствовал
-	(4, NULL), -- Щупальца шарятся в закаулках мозга Смотрящего на Луну
-	(5, 4); -- Затем у Смотрящего на Луну начались видения
+	('Оставить в покое', NULL), -- Крсталл оставил некоторых питекантропов в покое
+	('Сосредоточить внимание', NULL), -- Кристалл сосредоточил внимание на способных питекантропах
+	('Почувствовать', NULL), -- Смотрящий на Луну почувствовал
+	('Шариться в закаулках мозга', NULL), -- Щупальца шарятся в закаулках мозга Смотрящего на Луну
+	('Начаться', 4); -- Затем у Смотрящего на Луну начались видения
 
--- END;
+END;
